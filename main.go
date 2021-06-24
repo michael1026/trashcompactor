08907ecd3d1dae8ea632dd0d0f1c9d89c948f921
Michael Blake <michael1026@live.com>
2021-06-23 19:25:15 -0700

    move session management to different package, remove cachebusters

--- main.go
+++ main.go
@@ -3,21 +3,17 @@ package main
 import (
 	"bufio"
 	"crypto/tls"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
 	"os"
-	"strconv"
-	"strings"
 	"sync"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/michael1026/sessionManager"
 	"github.com/projectdiscovery/fastdialer/fastdialer"
 )
 
@@ -76,7 +72,7 @@ func main() {
 
 	flag.Parse()
 
-	jar := readCookieJson(*cookieFile)
+	jar := sessionManager.ReadCookieJson(*cookieFile)
 
 	client := buildHttpClient(*jar)
 
@@ -91,9 +87,9 @@ func main() {
 	wg.Wait()
 }
 
-func printUniqueContentURLs(url string, client *http.Client, wg *sync.WaitGroup, resources *SafeResources) {
+func printUniqueContentURLs(rawUrl string, client *http.Client, wg *sync.WaitGroup, resources *SafeResources) {
 	defer wg.Done()
-	req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest("GET", rawUrl, nil)
 	if err != nil {
 		return
 	}
@@ -116,7 +112,15 @@ func printUniqueContentURLs(url string, client *http.Client, wg *sync.WaitGroup,
 
 		doc.Find("script[src]").Each(func(index int, item *goquery.Selection) {
 			src, _ := item.Attr("src")
-			resource += src
+			srcurl, err := url.Parse(src)
+
+			if err != nil {
+				return
+			}
+
+			srcurl.RawQuery = ""
+
+			resource += srcurl.String()
 		})
 
 		if resources.Value(resource) {
@@ -124,143 +128,6 @@ func printUniqueContentURLs(url string, client *http.Client, wg *sync.WaitGroup,
 		}
 
 		resources.AddResource(resource)
-		fmt.Println(url)
+		fmt.Println(rawUrl)
 	}
-}
-
-func readCookiesFromString(s string) []*http.Cookie {
-	cookieStrings := strings.Split(s, ";")
-
-	for i, c := range cookieStrings {
-		cookieStrings[i] = strings.TrimSpace(c)
-	}
-
-	cookieCount := len(cookieStrings)
-	if cookieCount == 0 {
-		return []*http.Cookie{}
-	}
-	cookies := make([]*http.Cookie, 0, cookieCount)
-	for _, line := range cookieStrings {
-		parts := strings.Split(strings.TrimSpace(line), ";")
-		if len(parts) == 1 && parts[0] == "" {
-			continue
-		}
-		parts[0] = strings.TrimSpace(parts[0])
-		j := strings.Index(parts[0], "=")
-		if j < 0 {
-			continue
-		}
-		name, value := parts[0][:j], parts[0][j+1:]
-
-		value, ok := parseCookieValue(value, true)
-		if !ok {
-			continue
-		}
-		c := &http.Cookie{
-			Name:  name,
-			Value: value,
-			Raw:   line,
-		}
-		for i := 1; i < len(parts); i++ {
-			parts[i] = strings.TrimSpace(parts[i])
-			if len(parts[i]) == 0 {
-				continue
-			}
-
-			attr, val := parts[i], ""
-			if j := strings.Index(attr, "="); j >= 0 {
-				attr, val = attr[:j], attr[j+1:]
-			}
-			lowerAttr := strings.ToLower(attr)
-			val, ok = parseCookieValue(val, false)
-			if !ok {
-				c.Unparsed = append(c.Unparsed, parts[i])
-				continue
-			}
-			switch lowerAttr {
-			case "secure":
-				c.Secure = true
-				continue
-			case "httponly":
-				c.HttpOnly = true
-				continue
-			case "domain":
-				c.Domain = val
-				continue
-			case "max-age":
-				secs, err := strconv.Atoi(val)
-				if err != nil || secs != 0 && val[0] == '0' {
-					break
-				}
-				if secs <= 0 {
-					secs = -1
-				}
-				c.MaxAge = secs
-				continue
-			case "expires":
-				c.RawExpires = val
-				exptime, err := time.Parse(time.RFC1123, val)
-				if err != nil {
-					exptime, err = time.Parse("Mon, 02-Jan-2006 15:04:05 MST", val)
-					if err != nil {
-						c.Expires = time.Time{}
-						break
-					}
-				}
-				c.Expires = exptime.UTC()
-				continue
-			case "path":
-				c.Path = val
-				continue
-			}
-			c.Unparsed = append(c.Unparsed, parts[i])
-		}
-		cookies = append(cookies, c)
-	}
-	return cookies
-}
-
-func parseCookieValue(raw string, allowDoubleQuote bool) (string, bool) {
-	// Strip the quotes, if present.
-	if allowDoubleQuote && len(raw) > 1 && raw[0] == '"' && raw[len(raw)-1] == '"' {
-		raw = raw[1 : len(raw)-1]
-	}
-	return raw, true
-}
-
-func readCookieJson(filepath string) *cookiejar.Jar {
-	jar, err := cookiejar.New(nil)
-
-	if err != nil {
-		log.Fatal("Error reading cookie file")
-	}
-
-	if filepath == "" {
-		return jar
-	}
-
-	cookieFile, err := os.Open(filepath)
-	var cookies CookieInfo
-
-	if err != nil {
-		log.Fatal("Error creating cookie jar")
-	}
-
-	defer cookieFile.Close()
-
-	bytes, _ := ioutil.ReadAll(cookieFile)
-
-	json.Unmarshal(bytes, &cookies)
-
-	for rawUrl, cookieString := range cookies {
-		parsedUrl, err := url.Parse(rawUrl)
-
-		if err != nil {
-			continue
-		}
-
-		jar.SetCookies(parsedUrl, readCookiesFromString(cookieString))
-	}
-
-	return jar
 }
