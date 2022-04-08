3700f11514be5c638cf7b6ffaa12965464970479
Michael Blake <michael1026@live.com>
2022-04-08 00:14:37 -0700

    handle json output and json resources

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"bufio"
 	"crypto/tls"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"net"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
@@ -14,7 +17,6 @@ import (
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/michael1026/sessionManager"
-	"github.com/projectdiscovery/fastdialer/fastdialer"
 )
 
 type CookieInfo map[string]string
@@ -30,11 +32,12 @@ func (c *SafeResources) AddResource(key string) {
 	c.mu.Unlock()
 }
 
-func (c *SafeResources) AddAndPrintIfUnique(key string, url string) {
+func (c *SafeResources) AddAndPrintIfUnique(key string, url string, contentType string, jsonResult map[string]string) {
 	c.mu.Lock()
 	if !c.resources[key] {
 		fmt.Println(url)
 		c.resources[key] = true
+		jsonResult[url] = contentType
 	}
 	c.mu.Unlock()
 }
@@ -45,20 +48,16 @@ func (c *SafeResources) Value(key string) bool {
 	return c.resources[key]
 }
 
-func buildHttpClient(jar cookiejar.Jar) (c *http.Client) {
-	fastdialerOpts := fastdialer.DefaultOptions
-	fastdialerOpts.EnableFallback = true
-	dialer, err := fastdialer.NewDialer(fastdialerOpts)
-	if err != nil {
-		return
-	}
-
+func buildHttpClient(jar *cookiejar.Jar) (c *http.Client) {
 	transport := &http.Transport{
-		MaxIdleConns:      -1,
-		IdleConnTimeout:   time.Second,
+		MaxIdleConns:      30,
+		IdleConnTimeout:   3 * time.Second,
 		TLSClientConfig:   &tls.Config{InsecureSkipVerify: true},
 		DisableKeepAlives: true,
-		DialContext:       dialer.Dial,
+		DialContext: (&net.Dialer{
+			Timeout:   3 * time.Second,
+			KeepAlive: time.Second,
+		}).DialContext,
 	}
 
 	re := func(req *http.Request, via []*http.Request) error {
@@ -69,7 +68,7 @@ func buildHttpClient(jar cookiejar.Jar) (c *http.Client) {
 		Transport:     transport,
 		CheckRedirect: re,
 		Timeout:       time.Second * 3,
-		Jar:           &jar,
+		Jar:           jar,
 	}
 
 	return client
@@ -78,14 +77,16 @@ func buildHttpClient(jar cookiejar.Jar) (c *http.Client) {
 func main() {
 	resources := SafeResources{resources: make(map[string]bool)}
 	cookieFile := flag.String("C", "", "File containing cookie")
-	threads := flag.Int("t", 1, "Number of concurrent threads")
+	threads := flag.Int("t", 5, "Number of concurrent threads")
+	outputJson := flag.String("json", "", "Output as json")
+	jsonResult := make(map[string]string)
 
 	flag.Parse()
 
 	jar := sessionManager.ReadCookieJson(*cookieFile)
 	urls := make(chan string)
 
-	client := buildHttpClient(*jar)
+	client := buildHttpClient(jar)
 
 	wg := sync.WaitGroup{}
 	s := bufio.NewScanner(os.Stdin)
@@ -93,7 +94,7 @@ func main() {
 	for i := 0; i < *threads; i++ {
 		wg.Add(1)
 
-		go printUniqueContentURLs(urls, client, &wg, &resources)
+		go printUniqueContentURLs(urls, client, &wg, &resources, jsonResult)
 	}
 
 	for s.Scan() {
@@ -102,9 +103,24 @@ func main() {
 
 	close(urls)
 	wg.Wait()
+
+	if *outputJson != "" {
+		jsonFile, err := json.Marshal(jsonResult)
+
+		if err != nil {
+			fmt.Printf("Error marshalling JSON: %s\n", err)
+			return
+		}
+
+		err = ioutil.WriteFile(*outputJson, jsonFile, 0644)
+
+		if err != nil {
+			fmt.Printf("Error writing JSON to file: %s\n", err)
+		}
+	}
 }
 
-func printUniqueContentURLs(urls chan string, client *http.Client, wg *sync.WaitGroup, resources *SafeResources) {
+func printUniqueContentURLs(urls chan string, client *http.Client, wg *sync.WaitGroup, resources *SafeResources, jsonResult map[string]string) {
 	defer wg.Done()
 
 	for rawUrl := range urls {
@@ -121,28 +137,59 @@ func printUniqueContentURLs(urls chan string, client *http.Client, wg *sync.Wait
 
 		defer resp.Body.Close()
 
-		if resp.StatusCode == http.StatusOK && len(resp.Header.Get("content-type")) >= 9 && resp.Header.Get("content-type")[:9] == "text/html" {
-			doc, err := goquery.NewDocumentFromReader(resp.Body)
+		if resp.StatusCode == http.StatusOK {
 			resource := ""
-
-			if err != nil {
-				continue
-			}
-
-			doc.Find("script[src]").Each(func(index int, item *goquery.Selection) {
-				src, _ := item.Attr("src")
-				srcurl, err := url.Parse(src)
+			if len(resp.Header.Get("content-type")) >= 9 && resp.Header.Get("content-type")[:9] == "text/html" {
+				doc, err := goquery.NewDocumentFromReader(resp.Body)
 
 				if err != nil {
-					return
+					continue
 				}
 
-				srcurl.RawQuery = ""
+				doc.Find("script[src]").Each(func(index int, item *goquery.Selection) {
+					src, _ := item.Attr("src")
+					srcurl, err := url.Parse(src)
 
-				resource += srcurl.String()
-			})
+					if err != nil {
+						return
+					}
 
-			resources.AddAndPrintIfUnique(resource, rawUrl)
+					srcurl.RawQuery = ""
+
+					resource += srcurl.String()
+				})
+
+				resources.AddAndPrintIfUnique(resource, rawUrl, "text/html", jsonResult)
+			} else if len(resp.Header.Get("content-type")) >= 16 && resp.Header.Get("content-type")[:16] == "application/json" {
+				var resultMap map[string]interface{}
+				body, err := ioutil.ReadAll(resp.Body)
+
+				if err != nil {
+					continue
+				}
+
+				err = json.Unmarshal([]byte(body), &resultMap)
+
+				if err != nil {
+					continue
+				}
+
+				resource = mapKeysToString(resultMap)
+
+				resources.AddAndPrintIfUnique(resource, rawUrl, "application/json", jsonResult)
+			}
 		}
 	}
+}
+
+func mapKeysToString(jsonMap map[string]interface{}) string {
+	finalString := ""
+	for k := range jsonMap {
+		finalString += k
+	}
+	return finalString
+}
+
+func writeOutputToJson(result *SafeResources) {
+
 }
